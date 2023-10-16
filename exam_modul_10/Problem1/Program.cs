bf355b5b3fc67fdcd2be2f7a5f8321dc1b795d38
Teodor Dichev <teodichev@gmail.com>
2023-10-16 12:04:12 +0300

    Added modul 10 exam

---
+++ exam_modul_10/Problem1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+class Program
+{
+    static List<string> permutations = new List<string>();
+    static void Main(string[] args)
+    {
+        string[] elements = Console.ReadLine().Split(' ').ToArray();
+        int k = int.Parse(Console.ReadLine());
+        int[] output = new int[k];
+        string[] priorityElement = Console.ReadLine().Split(' ').ToArray();
+        FindSubsets(output, 0, 0, elements.Length, elements);
+
+        for (int j = 0; j < permutations.Count; j++)
+        {
+            List<string> words = permutations.ElementAt(j).Split(' ').ToList();
+            for (int i = priorityElement.Length - 1; i >= 0; i--)
+            {
+                for (int s = 0; s < words.Count; s++)
+                {
+                    if(words[s] == priorityElement[i])
+                    {
+                        words.RemoveAt(s);
+                        words.Insert(0, priorityElement[i]);
+                    }
+                }
+            }
+            Console.WriteLine(string.Join(' ', words));
+        }
+    }
+    static void FindSubsets(int[] arr, int index, int start, int end, string[] wordsArr)
+    {
+        if (index >= arr.Length)
+        {
+            string str = null;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                str += wordsArr[arr[i]];
+                str += " ";
+            }
+            permutations.Add(str);
+        }
+        else
+            for (int i = start; i < end; i++)
+            {
+                arr[index] = i;
+                FindSubsets(arr, index + 1, i + 1, end, wordsArr);
+            }
+    }
+}
---
+++ exam_modul_10/Problem2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] allWays = Array.ConvertAll(Console.ReadLine().Split(" "), Convert.ToInt32);
+        int cheapestWay = 0;
+        int nextMove = 1;
+
+        for (int i = 0; i < allWays.Length - 1; i += nextMove)
+        {
+            if (allWays[i] < allWays[i + 1])
+            {
+                nextMove = 1;
+                cheapestWay += allWays[i];
+            }
+            else
+            {
+                nextMove = 2;
+                cheapestWay += allWays[i + 1];
+            }
+        }
+        if (allWays.Length == 3) cheapestWay = allWays[1];
+
+        Console.WriteLine(cheapestWay);
+    }
+}
